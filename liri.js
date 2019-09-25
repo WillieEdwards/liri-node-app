5188a13079e251220790d706f76f68b8c814cb64
Willie Edwards <gammaray360@gmail.com>
2019-09-25 16:33:49 -0700

    updated liri.js with working movie-this and concert-this requests

--- liri.js
+++ liri.js
@@ -6,111 +6,116 @@ var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var fs = require("fs");
 
-var spotify = new Spotify({
-    id: keys.spotify.id,
-    secret: keys.spotify.secret,
-});
-
-var defaultMovie = "Blade";
-
+var spotify = new Spotify(keys.spotify);
 
 var action = process.argv[2];
 var value = process.argv[3];
 
 switch (action) {
-    case "concert-this":
-        getBands(value);
-        break;
-    case "spotify-this-song":
-        getSongs(value);
-        break;
-    case "movie-this":
-        if (value === "") {
-            value = defaultMovie;
-        }
-        getMovies(value);
-        break;
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
-    default:
-        break;
+  case "concert-this":
+    getBands(value)
+    break;
+  case "spotify-this-song":
+    //If user has not specified a song, use default
+    getSongs(value)
+    break;
+  case "movie-this":
+    //If user has not specified a movie, use default
+    if (value === "") {
+      value = defaultMovie;
+    }
+    getMovies(value)
+    break;
+  case "do-what-it-says":
+    doWhatItSays()
+    break;
+  default:
+    break;
 }
 
-function getBand(artist) {
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=")
-        .then(function (response) {
-            console.log("Venue: " response.data[0].venue.name);
-            console.log("Location: ", response.data[0].venue.city);
-            var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
-        });
-        .catch (function (error) {
-        console.log(error)
+function getBands(artist) {
+
+  var artistUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+  axios.get(artistUrl)
+    .then(function (response) {
+
+      var artistResult = response.data
+      var eventDate = moment(artistResult[0].datetime).format('MM/DD/YYYY');
+
+      console.log("Venue name:", artistResult[0].venue.name);
+      console.log("Location:", artistResult[0].venue.city);
+      console.log("Date:", eventDate);
     })
+    .catch(function () {
+      console.log("Please enter a valid artist/band name...");
+    });
 }
 
 function getSongs(songName) {
-    if (songName === "") {
-        songName "Panama";
-    }
 
-spotify.search({
-    type: "track",
-    query: songName
-}, function (err, data) {
+  //If user has not specified a song, default to "The Sign" by Ace of Bass
+  if (songName === "") {
+    songName = "The Sign";
+  }
+
+  spotify.search({ type: "track", query: songName }, function (err, data) {
     if (err) {
-        return console.log("Error occurred: " + err);
+      return console.log("Error occurred: " + err);
     }
 
-    console.log("Artists: ", data.tracks.items[0].album.artists[0].name);
-    console.log("Preview Link: ", data.tracks.items[0].preview_url);
-    console.log("Album Name: ", data.tracks.items[0].album.name);
-})
+    //Artist(s)
+    console.log("Artist/Band: ", data.tracks.items[0].album.artists[0].name)
+    //Song name
 
-
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    //A preview URL of the song from Spotify
+    console.log("Preview URL: ", data.tracks.items[0].preview_url)
+    //The album that the song is from
+    console.log("Album Name: ", data.tracks.items[0].album.name)
+  });
+}
 
 function getMovies(movieName) {
-    axios.get(queryUrl).then(function (response) {
-        var results = `
-        Title of the movie: ${response.data.Title}
-        Year the movie came out: ${response.data.Year}
-        IMDB Rating of the movie: ${response.data.Rated}
-        Rotten Tomatoes Rating of the movie: ${response.data.Ratings[1].Value}
-        Country where the movie was produced: ${response.data.Country}
-        Language of the movie: ${response.data.Language}
-        Plot of the movie: ${response.data.Plot}
-        Actors in the movie: ${response.data.Actors}`;
-        console.log(results)
-    });
 
-    .catch (function (error) {
-        console.log(error);
+  var movieUrl = "http://www.omdbapi.com/?apikey=42518777&t=" + movieName;
+
+  axios.get(movieUrl).then(function (response) {
+
+    var movieResult = response.data;
+
+    console.log("Title: ", movieResult.Title);
+    console.log("Year: ", movieResult.Year);
+    console.log("IMDb Rating: ", movieResult.Rated);
+    console.log("Rotten Tomatoes Rating: ", movieResult.Ratings[1].Value);
+    console.log("Country: ", movieResult.Country);
+    console.log("Language: ", movieResult.Language);
+    console.log("Plot: ", movieResult.Plot);
+    console.log("Actors: ", movieResult.Actors);
+
+  })
+    .catch(function () {
+      console.log("Please enter a valid movie title...");
     });
-    if (movieName === "Blade") {
-        console.log("----------------");
-        console.log("If you haven't seen 'Blade,' then you should: https://www.imdb.com/title/tt0120611/");
-        console.log("It's on Netflix on 10/01/2019!");
-    };
+  //Response if user does not type in a movie title
 }
 
 function doWhatItSays() {
-    fs.readFile("random.txt", "utf8", function (err, data) {
-        data = data.split(",");
-        var action = data[0];
-        var value = data[1];
-        switch (action) {
-            case "concert-this":
-                getBands(value);
-                break;
-            case "spotify-this-song":
-                getSongs(value);
-                break;
-            case "movie-this":
-                getMovies(value)
-                break;
-            default:
-                break;
-        }
-    })
+  fs.readFile("random.txt", "utf8", function (data) {
+    data = data.split(",");
+    var action = data[0]
+    var value = data[1]
+    switch (action) {
+      case "concert-this":
+        getBands(value)
+        break;
+      case "spotify-this-song":
+        getSongs(value)
+        break;
+      case "movie-this":
+        getMovies(value)
+        break;
+      default:
+        break;
+    }
+  });
 }
